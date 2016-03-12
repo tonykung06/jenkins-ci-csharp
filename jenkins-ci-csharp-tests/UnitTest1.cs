5cb256ba4cceb797dd429a31b9e7ee20a9d4c0c1
tonykung <tonykung06@gmail.com>
2016-03-13 01:41:16 +0800

    initial commit

---
+++ jenkins-ci-csharp-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using jenkins_ci_csharp;
+
+namespace jenkins_ci_csharp_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+    }
+}
---
+++ jenkins-ci-csharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jenkins_ci_csharp
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
